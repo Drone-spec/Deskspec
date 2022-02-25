0ee0c04a0a1aa167494655e56704edc8f6de55d5
Drone <patrick10jenkins@gmail.com>
2022-02-25 13:27:09 -0500

    Create runner.sh
    Pushed to commit

---
+++ runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd .config
+mkdir nvim
+git clone https://github.com/drone-spec/Deskspec/init.vim
+cd ~
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/nvim
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+cd Downloads
+git clone https://github.com/neovim/neovim
+sudo apt-get install python3-pip
+cd neovim
+make
+sudo make install
+sudo su
+curl -sL install-node.vercel.app/lts | bash
+exit
