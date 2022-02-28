0c9690926408d038f7721c806070774d885d67fe
Drone <patrick10jenkins@gmail.com>
2022-02-28 09:23:20 -0500

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -9,10 +9,10 @@ source $HOME/.cargo/nvim
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 cd Downloads
 git clone https://github.com/neovim/neovim
-sudo apt-get install python3-pip
-cd neovim
-make
+cd neovim && 
+sudo make
+make CMAKE_BUILD_TYPE=Release
 sudo make install
-sudo su
+sudo apt-get install python3-pip
+sudo curl -sL install-node.vercel.app/lts | bash
 curl -sL install-node.vercel.app/lts | bash
-exit
