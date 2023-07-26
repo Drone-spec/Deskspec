1e35979fbd7a45dd87727d59ce4f6af321dd71e1
Drone <patrick10jenkins@gmail.com>
2023-07-25 23:16:53 -0400

    Update keybind.lua

--- nvim/lua/drone/keybind.lua
+++ nvim/lua/drone/keybind.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>gs", vim.cmd.LazyGit)
-vim.keymap.set("n", "<leader>uf", require("lazyvim.plugins.lsp.format").toggle, { desc = "Toggle format on Save" })
+
 
